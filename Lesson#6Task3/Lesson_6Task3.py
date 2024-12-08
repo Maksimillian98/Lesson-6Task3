30cb01c9cbcd58718bc4b37a4fc790f4b25879e8
maxstar <maksimgavura78@gmail.com>
2024-12-09 00:47:57 +0200

    Добавьте файлы проекта.

---
+++ Lesson#6Task3/Lesson_6Task3.py
@@ -0,0 +1,7 @@
+﻿A = int(input('vvedite celoe 4islo A: '))
+B = int(input('vvedite celoe 4islo B >= 4islu A: '))
+if A > B: 
+    print('4islo B dolzno byt bolwe ili ravna 4islu A')
+else:
+    count = [str(i) for i in range(A, B) if i % 2 == 0]
+    print(' '.join(count))
